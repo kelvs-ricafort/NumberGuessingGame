a322ae0c06c101bb777e994267ec3ca96fb890be
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-21 23:18:10 +0800

    Splash Screen and Adding Animation Components

---
+++ app/src/main/java/com/newtechieblog/wordpress/views/numberguessinggame/SplashActivity.java
@@ -0,0 +1,13 @@
+package com.newtechieblog.wordpress.views.numberguessinggame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
