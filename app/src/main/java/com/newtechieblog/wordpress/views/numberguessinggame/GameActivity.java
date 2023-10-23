ad8ebd3c759fcaf8e1395ee898e07f7c910c0d07
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-23 15:49:26 +0800

    Game Page Design

--- app/src/main/java/com/newtechieblog/wordpress/views/numberguessinggame/GameActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/numberguessinggame/GameActivity.java
@@ -3,12 +3,33 @@ package com.newtechieblog.wordpress.views.numberguessinggame;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
+
+    private TextView textViewLast, textViewWrite, textViewHint;
+    private Button btnConfirm;
+    private EditText editTextGuess;
+
+    boolean twoDigits, threeDigits, fourDigits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        textViewLast = findViewById(R.id.textViewLast);
+        textViewWrite = findViewById(R.id.textViewWrite);
+        textViewHint = findViewById(R.id.textViewHint);
+
+        btnConfirm = findViewById(R.id.btnConfirm);
+        editTextGuess = findViewById(R.id.editTextGuess);
+
+        twoDigits = getIntent().getBooleanExtra("two", false);
+        threeDigits = getIntent().getBooleanExtra("three", false);
+        fourDigits = getIntent().getBooleanExtra("four", false);
+
     }
 }
