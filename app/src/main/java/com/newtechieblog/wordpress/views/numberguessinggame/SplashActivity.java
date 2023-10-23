7fb5a40270a716a4efc7e6b9f4c8c3ce51d47654
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-23 14:37:38 +0800

    Home Page Design

--- app/src/main/java/com/newtechieblog/wordpress/views/numberguessinggame/SplashActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/numberguessinggame/SplashActivity.java
@@ -2,12 +2,46 @@ package com.newtechieblog.wordpress.views.numberguessinggame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+    private TextView textView;
+
+    Animation animationImage, animationText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        textView = findViewById(R.id.textViewLabel);
+        imageView = findViewById(R.id.imageViewLogo);
+
+        animationImage = AnimationUtils.loadAnimation(this, R.anim.image_animation);
+        animationText = AnimationUtils.loadAnimation(this, R.anim.text_animation);
+
+        imageView.setAnimation(animationImage);
+        textView.setAnimation(animationText);
+
+        new CountDownTimer(5000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }.start();
     }
 }
